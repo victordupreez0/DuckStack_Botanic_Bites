d9e7193095b1f238c674847eb5de8876967147c6
wrapperik <159801009+wrapperik@users.noreply.github.com>
2025-09-01 22:40:01 +0200

    Added Subtle Paralax Effect to header

--- Frontend/src/Components/header.jsx
+++ Frontend/src/Components/header.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import BlurText from '../UI/headerText'
+import BlurText from '../UI/headerText';
+import Flytrap from '../assets/flytrap.png'; // replace with your own image path
 
 const Header = () => {
   return (
-  <header
-  className="w-full flex  flex-col items-center h-[100vh] bg-cover bg-center"
-  style={{ backgroundImage: 'url(/cover.png)' }}
->
-  <BlurText
-    text="Botanic Bites"
-    delay={150}
-    animateBy="words"
-    direction="top"
-    className="text-[80px] font-bold md:text-[150px] text-center leading-[1.1] mt-20 inline-block"
-  />
-</header>);
+    <header
+      className="relative w-full flex flex-col items-center h-[95vh] bg-black bg-cover bg-center"
+    >
+      <BlurText
+        text="Botanic Bites"
+        delay={150}
+        animateBy="words"
+        direction="top"
+        className="text-[80px] font-bold md:text-[150px] text-center leading-[1.1] mt-20 z-10"
+      />
+
+      {/* Venus flytrap image overlapping text */}
+      <img
+        src={Flytrap}
+        alt="Venus Flytrap"
+        className="w-[500px] md:w-[1080px] absolute bottom-0 transform translate-y-[0px] z-50 drop-shadow-[0_20px_30px_rgba(1,1,1,50)]"
+      />
+    </header>
+  );
 };
 
 export default Header;
