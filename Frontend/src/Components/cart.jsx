25b6072484ad85ef2bc5a8ac4453b4af4172ee39
wrapperik <159801009+wrapperik@users.noreply.github.com>
2025-09-04 22:40:52 +0200

    Added Animation to Cart Page

--- Frontend/src/Components/cart.jsx
+++ Frontend/src/Components/cart.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import CartProduct from '../UI/Cart/cartProduct';
-import CartHeader from './cartHeader'
+import CartHeader from './cartHeader';
 import CartSummary from './cartSummary';
+import { motion } from "framer-motion";
 
 function Cart() {
     return (
         <>
        
         <CartHeader />
-        <div className="flex flex-col md:flex-row gap-8 w-full max-w-5xl mx-auto mt-10 mb-20">
-            <div className="flex flex-col flex-1 items-center space-y-4">
-                <CartProduct />
-                <CartProduct />
-                <CartProduct />
-            </div>
-            <div className="flex-1 md:max-w-xs w-full md:sticky md:top-24">
-                <CartSummary />
-            </div>
-        </div>
+                <motion.div
+                    className="flex flex-col md:flex-row gap-8 w-full max-w-5xl mx-auto mt-10 mb-20"
+                    initial={{ opacity: 0, y: 40 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.6, ease: "easeOut" }}
+                >
+                    <div className="flex flex-col flex-1 items-center space-y-4">
+                        <CartProduct />
+                        <CartProduct />
+                        <CartProduct />
+                    </div>
+                    <div className="flex-1 md:max-w-xs w-full md:sticky md:top-24">
+                        <CartSummary />
+                    </div>
+                </motion.div>
         </>
     );
 }
